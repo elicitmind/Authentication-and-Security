1dfe75603c7056a03725653db07820d4c3dee01b
elicitmind <elicitmind@gmail.com>
2021-06-26 07:24:38 +0200

    server run locally

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-//jshint esversion:6
+const express = require("express")
+const ejs = require("ejs")
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({
+    extended: true
+}))
+
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("server running on 3k")
+})
