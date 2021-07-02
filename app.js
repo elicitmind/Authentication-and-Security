a6268cc0ac068c64530851c16eac766dc388c775
elicitmind <elicitmind@gmail.com>
2021-07-02 06:45:46 +0200

    bootstrap button, Oauth for google and facebook, post the knowledge, print on main webpage
    These
    shell
    commands are defined internally.  Type `help' to see this list.
    Type `help name' to find out more about the function `name'.
    Use `info bash' to find out more about the shell in general.
    Use `man -k' or `info' to find out more about commands not in this list.
    
    A star (*) next to a name means that the command is disabled.
    
     job_spec [&]                            history [-c] [-d offset] [n] or hist>
     (( expression ))                        if COMMANDS; then COMMANDS; [ elif C>
     . filename [arguments]                  jobs [-lnprs] [jobspec ...] or jobs >
     :                                       kill [-s sigspec | -n signum | -sigs>
     [ arg... ]                              let arg [arg ...]
     [[ expression ]]                        local [option] name[=value] ...
     alias [-p] [name[=value] ... ]          logout [n]
     bg [job_spec ...]                       mapfile [-d delim] [-n count] [-O or>
     bind [-lpsvPSVX] [-m keymap] [-f file>  popd [-n] [+N | -N]
     break [n]                               printf [-v var] format [arguments]
     builtin [shell-builtin [arg ...]]       pushd [-n] [+N | -N | dir]
     caller [expr]                           pwd [-LPW]
     case WORD in [PATTERN [| PATTERN]...)>  read [-ers] [-a array] [-d delim] [->
     cd [-L|[-P [-e]] [-@]] [dir]            readarray [-n count] [-O origin] [-s>
     command [-pVv] command [arg ...]        readonly [-aAf] [name[=value] ...] o>
     compgen [-abcdefgjksuv] [-o option] [>  return [n]
     complete [-abcdefgjksuv] [-pr] [-DE] >  select NAME [in WORDS ... ;] do COMM>
     compopt [-o|+o option] [-DE] [name ..>  set [-abefhkmnptuvxBCHP] [-o option->
     continue [n]                            shift [n]
     coproc [NAME] command [redirections]    shopt [-pqsu] [-o] [optname ...]
     declare [-aAfFgilnrtux] [-p] [name[=v>  source filename [arguments]
     dirs [-clpv] [+N] [-N]                  suspend [-f]
     disown [-h] [-ar] [jobspec ... | pid >  test [expr]
     echo [-neE] [arg ...]                   time [-p] pipeline
     enable [-a] [-dnps] [-f filename] [na>  times
     eval [arg ...]                          trap [-lp] [[arg] signal_spec ...]
     exec [-cl] [-a name] [command [argume>  true
     exit [n]                                type [-afptP] name [name ...]
     export [-fn] [name[=value] ...] or ex>  typeset [-aAfFgilnrtux] [-p] name[=v>
     false                                   ulimit [-SHabcdefiklmnpqrstuvxPT] [l>
     fc [-e ename] [-lnr] [first] [last] o>  umask [-p] [-S] [mode]
     fg [job_spec]                           unalias [-a] name [name ...]
     for NAME [in WORDS ... ] ; do COMMAND>  unset [-f] [-v] [-n] [name ...]
     for (( exp1; exp2; exp3 )); do COMMAN>  until COMMANDS; do COMMANDS; done
     function name { COMMANDS ; } or name >  variables - Names and meanings of so>
     getopts optstring name [arg]            wait [-n] [id ...]
     hash [-lr] [-p pathname] [-dt] [name >  while COMMANDS; do COMMANDS; done
     help [-dms] [pattern ...]               { COMMANDS ; }

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate")
+const FacebookStrategy = require("passport-facebook")
 
 
 app.set("view engine", "ejs")
@@ -36,7 +37,9 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    knowledge: String
 })
 
 userSchema.plugin(passportLocalMongoose)
@@ -46,20 +49,44 @@ const User = new mongoose.model("User", userSchema)
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets"
+        // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile)
+        User.findOrCreate({
+            googleId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+passport.use(new FacebookStrategy({
+        clientID: process.env.FB_APP_ID,
+        clientSecret: process.env.FB_APP_SECRET,
+        callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({
+            facebookId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
 ));
 
 
@@ -68,17 +95,68 @@ app.get("/", (req, res) => {
     res.render("home")
 })
 
-app.get("/auth/google", (req, res) => {
-    passport.authenticate("google", { scope: ["profile"]} )
-})
+app.get("/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile"]
+    }));
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", {
+        failureRedirect: "/login"
+    }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect("/secrets");
+    });
+
+app.get("/auth/facebook",
+    passport.authenticate("facebook"));
+
+app.get("/auth/facebook/callback",
+    passport.authenticate("facebook", {
+        failureRedirect: "/login"
+    }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect("/secrets");
+    });
 
 
 app.get("/secrets", (req, res) => {
-    if  (req.isAuthenticated()) {
-        res.render("secrets")
+  User.find({"knowledge":{$ne: null}}, (err, results)=>{
+      if(err) {
+          consol.log(err)
+      } else {
+          if (results) {
+              res.render("secrets", {usersWithKnowledge: results} )
+          }
+      }
+  })
+})
+
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("submit")
     } else {
         res.redirect("/login")
     }
+})
+
+app.post("/submit", (req, res) => {
+    const submittedKnowledge = req.body.knowledge
+
+    User.findById(req.user.id, (err, results) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (results) {
+                results.knowledge = submittedKnowledge
+                results.save(() => {
+                    res.redirect("/secrets")
+                })
+            }
+        }
+    })
 })
 
 app.get("/login", (req, res) => {
@@ -86,7 +164,7 @@ app.get("/login", (req, res) => {
 })
 
 app.get("/register", (req, res) => {
-res.render("register")
+    res.render("register")
 })
 
 app.get("/logout", (req, res) => {
@@ -100,7 +178,7 @@ app.post("/register", (req, res) => {
     User.register({
         username: req.body.username
     }, req.body.password, (err, account) => {
-        if(err) {
+        if (err) {
             console.log(err)
             res.redirect("/register")
         } else {
@@ -112,22 +190,21 @@ app.post("/register", (req, res) => {
 })
 
 app.post("/login", (req, res) => {
-//new user
+    //new user
     const user = new User({
         username: req.body.username,
         password: req.body.password
     })
-//method from passport
+    //method from passport
     req.login(user, (err) => {
         if (err) {
             console.log(err)
         } else {
-            passport.authenticate("local")(req, res, ()=>{
+            passport.authenticate("local")(req, res, () => {
                 res.redirect("/secrets")
             })
         }
-    }
-    )
+    })
 })
 
 
